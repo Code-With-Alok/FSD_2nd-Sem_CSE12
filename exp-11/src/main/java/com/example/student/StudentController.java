2f20bb053440878496ad2f9b7364b76e98449576
Alok Sharan <aloksharan168@gmail.com>
2026-06-19 19:20:22 +0530

    Added Experiment 11: Creating REST API using Spring Boot

---
+++ exp-11/src/main/java/com/example/student/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.student;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class StudentController {
+    private Student student;
+
+    // Welcome endpoint
+    @GetMapping
+    public String welcomeMessage() {
+        return "Welcome to Student Management REST API";
+    }
+
+    // GET endpoint to retrieve student details
+    @GetMapping("/student")
+    public ResponseEntity<Student> getStudent() {
+        if (student == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    // POST endpoint to save student details
+    @PostMapping("/student")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        this.student = student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+}
